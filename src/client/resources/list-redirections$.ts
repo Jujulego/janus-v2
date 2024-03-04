8c05b8df63a081c20c177f073d4687282fe39097
Julien Capellari <julien.capellari@gmail.com>
2024-03-04 23:19:55 +0100

    feat: use fragment

--- src/client/resources/list-redirections$.ts
+++ src/client/resources/list-redirections$.ts
@@ -1,32 +1,49 @@
-import { each$, pipe$, readable$, resource$ } from 'kyrielle';
+import { each$, observable$, pipe$, readable$, resource$ } from 'kyrielle';
 
 import { JanusClient } from '../janus-client.ts';
-import { graphql } from '../gql/index.ts';
+import { graphql, useFragment } from '../gql/index.ts';
+
+// Fragments
+export const RedirectionItem = graphql(/* GraphQL */ `
+  fragment RedirectionItem on Redirection {
+    id
+    url
+    outputs {
+      id
+      name
+      target
+      enabled
+      changeOrigin
+      secure
+      ws
+    }
+  }
+`);
 
 // Queries
 const ListRedirectionsQuery = graphql(/* GraphQL */ `
-    query ListRedirections {
-        redirections {
-            id
-            url
-            outputs {
-                id
-                name
-                target
-                enabled
-                changeOrigin
-                secure
-                ws
-            }
-        }
+  query ListRedirections {
+    redirections {
+      ...RedirectionItem
     }
+  }
+`);
+
+const ListRedirectionsStream = graphql(/* GraphQL */ `
+  subscription ListRedirectionsStream {
+    redirections {
+      ...RedirectionItem
+    }
+  }
 `);
 
 export function listRedirections$(client: JanusClient) {
   return pipe$(
     resource$()
       .add(readable$((signal) => client.send(ListRedirectionsQuery, { signal })))
+      .add(observable$((observer, signal) => client.subscribe(observer, ListRedirectionsStream, { signal })))
       .build(),
-    each$(({ data }) => data?.redirections ?? [])
+    each$(({ data }) => data?.redirections ?? []),
+    each$((redirections) => redirections.map((r) => useFragment(RedirectionItem, r)))
   );
 }
