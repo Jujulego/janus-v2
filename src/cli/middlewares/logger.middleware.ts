b20229991f687fe419f0ccd8201f7e20bec93aa4
Julien Capellari <julien.capellari@gmail.com>
2024-03-20 23:48:08 +0100

    feat: setup a log gateway

--- src/cli/middlewares/logger.middleware.ts
+++ src/cli/middlewares/logger.middleware.ts
@@ -1,6 +1,16 @@
 import { inject$ } from '@jujulego/injector';
 import { defineQuickFormat, q$, qarg, qerror, qprop, qwrap } from '@jujulego/quick-tag';
-import { Log, logDebugFilter$, logDelay$, LogLevel, LogLevelKey, qLogDelay, toStderr, WithDelay } from '@kyrielle/logger';
+import {
+  Log,
+  logDebugFilter$,
+  logDelay$,
+  LogGateway,
+  LogLevel,
+  LogLevelKey,
+  qLogDelay,
+  toStderr,
+  WithDelay
+} from '@kyrielle/logger';
 import { ColorName, ModifierName } from 'chalk';
 import { chalkTemplateStderr } from 'chalk-template';
 import { filter$, flow$ } from 'kyrielle';
@@ -37,15 +47,18 @@ export function loggerMiddleware(parser: Argv) {
     .middleware(async (args) => {
       const logLevel = args.verbose ? LogLevel[args.verbose] : LogLevel.info;
       const logger = inject$(CliLogger);
+      const logGateway = inject$(LogGateway);
 
       flow$(
         logger,
         filter$((log) => log.level >= logLevel),
         logDebugFilter$(),
         logDelay$(),
-        toStderr(qwrap(chalkTemplateStderr)
-          .fun`#?:${qprop('label')}{grey [${q$}]} ?#{${logColor} ${qprop('message')} {grey +${qLogDelay(qarg<WithDelay>())}}#?:${qerror(qprop<Log>('error'))}${os.EOL}${q$}?#}`
-        )
+        logGateway,
       );
+
+      logGateway.connect('stderr', toStderr(qwrap(chalkTemplateStderr)
+        .fun`#?:${qprop('label')}{grey [${q$}]} ?#{${logColor} ${qprop('message')} {grey +${qLogDelay(qarg<WithDelay>())}}#?:${qerror(qprop<Log>('error'))}${os.EOL}${q$}?#}`
+      ));
     });
 }
