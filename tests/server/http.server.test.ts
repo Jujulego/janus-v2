e6a3af95cc8afa09d425357a18bc03820ddca66d
Julien Capellari <julien.capellari@gmail.com>
2024-05-20 00:42:41 +0200

    test: HttpServer

--- tests/server/http.server.test.ts
+++ tests/server/http.server.test.ts
@@ -2,8 +2,9 @@ import { Logger, logger$ } from '@kyrielle/logger';
 import createHttpError from 'http-errors';
 import { ServerResponse } from 'node:http';
 import request from 'supertest';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, type Mock, vi } from 'vitest';
 
+import { version } from '../../package.json' assert { type: 'json' };
 import { HttpServer } from '@/src/server/http.server.js';
 import { DEFAULT_CONFIG } from '@/tests/utils.js';
 import { serverStore } from '@/src/server/store/server.store.js';
@@ -21,7 +22,7 @@ beforeEach(() => {
 describe('HttpServer.listen', () => {
   it('should call internal server listen method', async () => {
     vi.spyOn(server.server, 'listen')
-      .mockImplementation(function (_, cb) {
+      .mockImplementation((_, cb) => {
         cb && cb();
         setTimeout(() => server.server.emit('listening'));
         return server.server;
@@ -30,6 +31,43 @@ describe('HttpServer.listen', () => {
     await server.listen(DEFAULT_CONFIG);
 
     expect(server.server.listen).toHaveBeenCalledWith(3000, expect.any(Function));
+  });
+});
+
+describe('HttpServer.close', () => {
+  it('should call internal server close method', async () => {
+    vi.spyOn(server.server, 'close')
+      .mockImplementation((cb) => {
+        setTimeout(() => cb!());
+        return server.server;
+      });
+
+    await server.close();
+
+    expect(server.server.close).toHaveBeenCalledOnce();
+  });
+
+  it('should throw if an error happened', async () => {
+    vi.spyOn(server.server, 'close')
+      .mockImplementation((cb) => {
+        setTimeout(() => cb!(new Error('This will continue forever!')));
+        return server.server;
+      });
+
+    await expect(server.close()).rejects.toEqual(new Error('This will continue forever!'));
+
+    expect(server.server.close).toHaveBeenCalledOnce();
+  });
+});
+
+describe('HttpServer => health endpoint', () => {
+  it('should respond with version and pid', async () => {
+    await request(server.server)
+      .get('/_janus/health')
+      .expect(200, {
+        pid: process.pid,
+        version
+      });
   });
 });
 
@@ -80,6 +118,50 @@ describe('HttpServer => ProxyServer', () => {
 
     expect(server.proxy.handleRequest).toHaveBeenCalledWith(
       expect.objectContaining({ url: '/test' }),
+      expect.any(ServerResponse)
+    );
+  });
+});
+
+describe('HttpServer => YogaServer', () => {
+  beforeEach(() => {
+    Object.assign(server, {
+      yoga: {
+        handle: vi.fn()
+      }
+    });
+  });
+
+  it('should pass request to yoga', async () => {
+    (vi.mocked(server.yoga.handle) as unknown as Mock<[unknown, ServerResponse], void>)
+      .mockImplementation(async (req, res) => {
+        res.write('cool');
+        res.end();
+      });
+
+    await request(server.server)
+      .get('/_janus')
+      .expect(200, 'cool');
+
+    expect(server.yoga.handle).toHaveBeenCalledWith(
+      expect.objectContaining({ url: '/_janus' }),
+      expect.any(ServerResponse)
+    );
+  });
+
+  it('should handle errors from yoga', async () => {
+    vi.mocked(server.yoga.handle)
+      .mockRejectedValue(new Error('Failure'));
+
+    await request(server.server)
+      .get('/_janus')
+      .expect(500, {
+        status: 500,
+        message: 'Failure'
+      });
+
+    expect(server.yoga.handle).toHaveBeenCalledWith(
+      expect.objectContaining({ url: '/_janus' }),
       expect.any(ServerResponse)
     );
   });
