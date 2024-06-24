583be8a1bbabe24c47c02ff6cd7e3f94c4836942
Julien Capellari <julien.capellari@gmail.com>
2024-06-24 11:26:56 +0200

    fix: lint errors

--- src/cli/inked.tsx
+++ src/cli/inked.tsx
@@ -11,31 +11,30 @@ export function inked<P, R>(stepper: InkedStepper<P, R>): InkedComponent<P, R> {
   return async (props: P): Promise<R> => {
     const controller = new AbortController();
 
-    const app = render(<StaticLogs/>);
+    const app = render(<StaticLogs />);
     app.waitUntilExit().then(() => {
       controller.abort();
     });
 
     try {
       const generator = stepper(props, controller);
+      let result = await generator.next();
 
-      while (true) {
-        const result = await generator.next();
+      while (!result.done) {
+        app.rerender(
+          <>
+            <StaticLogs/>
+            { result.value }
+          </>
+        );
 
-        if (!result.done) {
-          app.rerender(
-            <>
-              <StaticLogs/>
-              { result.value }
-            </>
-          );
-        } else {
-          return result.value;
-        }
+        result = await generator.next();
       }
+
+      return result.value;
     } finally {
       app.clear();
       app.unmount();
     }
-  }
+  };
 }
